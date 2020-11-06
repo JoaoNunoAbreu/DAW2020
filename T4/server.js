666484aa2ce12f3c3adfa6e5aa94837fa7d0e79f
João Nuno Abreu <joao.nuno.escola@gmail.com>
2020-11-06 18:43:09 +0000

    t4 v1.1

--- T4/server.js
+++ T4/server.js
@@ -3,6 +3,7 @@ var fs = require('fs')
 
 http.createServer(function(req,res){
     console.log(req.method + " - " + req.url)
+    // Regex para apanhar os números
     if(req.url.match(/\/arqs\/index.html/)){
         fs.readFile("./arqs/index.html",function(err,data){
             res.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'})
@@ -10,20 +11,20 @@ http.createServer(function(req,res){
             res.end()
         })
     }
-    else if (req.url.match(/\/arqs\/arq[0-9]*.html/)){
-        // Regex para apanhar os números
-        var num = req.url.match(/\d+/)[0]
-        
-        fs.readFile("./arqs/arq" + num + ".html",function(err,data){
+    else {
+        var num = parseInt(req.url.match(/\d+/)[0])
+        if (req.url.match(/\/arqs\/arq[0-9]*.html/) && num <= 122){
+            fs.readFile("./arqs/arq" + num + ".html",function(err,data){
+                res.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'})
+                res.write(data)
+                res.end()
+            })
+        }
+        else{
             res.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'})
-            res.write(data)
-            res.end()
-        })
-    }
-    else{
-        res.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'})
-        res.write("<p>O URL não corresponde ao esperado.</p>")
-        res.end() 
+            res.write("<p>O URL não corresponde ao esperado.</p>")
+            res.end() 
+        }
     }
 }).listen(7777)
 console.log("Servidor à escuta na porta 7777!")
